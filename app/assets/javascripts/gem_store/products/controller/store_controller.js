d31035d9d068b82ae21733ef95788e9f693e63df
Jerel Navarrete <navarrete.jerel@gmail.com>
2014-10-28 09:46:38 -0700

    fixed #scope to

--- app/assets/javascripts/gem_store/products/controller/store_controller.js
+++ app/assets/javascripts/gem_store/products/controller/store_controller.js
@@ -5,12 +5,3 @@ app.controller('StoreController', ['$http', "$scope", function($http, $scope){
       $scope.products = data;
   });
 }]);
-
-app.controller('ReviewController', function($scope) {
-  $scope.review = {};
-
-  $scope.addReview = function(product) {
-    product.reviews.push($scope);
-    $scope.review = {};
-  };
-});
